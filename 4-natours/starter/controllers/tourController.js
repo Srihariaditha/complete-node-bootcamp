f22afbcb57af256b1fe97494939757d56c70c590
Srihariaditha <adithasrihari@gmail.com>
2023-05-23 22:34:53 +0530

    Adding mongoose and models

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,24 +1,27 @@
-const fs = require('fs');
-const path = require('path');
+// const path = require('path');
+// const fs = require('fs');
 
-const toursJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..','dev-data', 'data', 'tours.json')));
+const Tour = require('./../models/Tour');
+
+// const toursJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..','dev-data', 'data', 'tours.json')));
 
 exports.getAllTours = (req, res) => {
-    console.log(req.requestTime)
-    res.status(200).json({data: toursJson, status: "OK", results: toursJson.length});
-}
+//   console.log(req.requestTime);
+  res.status(200).json({ status: 'OK' });
+};
 
 exports.getTour = (req, res) => {
-    res.status(200).json({data: toursJson, status: "OK", results: toursJson.length});
-}
+  res.status(200).json({ status: 'OK' });
+};
 
 exports.createTour = (req, res) => {
-    res.status(201).json({data: "Created"});
-}
+  res.status(201).json({ data: 'Created' });
+};
 
 exports.updateTour = (req, res) => {
-    res.status(200).json({data: "updated"});
-}
+  res.status(200).json({ data: 'updated' });
+};
+
 exports.deleteTour = (req, res) => {
-    res.status(204).json({data: "Deleted"});
-}
+  res.status(204).json({ data: 'Deleted' });
+};
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,21 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 // console.log(process.env);
 
 const app = require('./app');
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connected'));
 
 //SERVER
 const port = process.env.PORT || 8000;
